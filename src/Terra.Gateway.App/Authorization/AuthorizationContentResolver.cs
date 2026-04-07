903e142a2757b0c8455d91287377300ea5a27fe0
voskakiscite <kvoskakis@cite.gr>
2026-04-07 14:41:30 +0300

    remove unused authorization methods

--- src/Terra.Gateway.App/Authorization/AuthorizationContentResolver.cs
+++ src/Terra.Gateway.App/Authorization/AuthorizationContentResolver.cs
@@ -1,8 +1,6 @@
 ﻿using Cite.Tools.Auth.Claims;
-using Cite.Tools.Common.Extensions;
 using Cite.Tools.Data.Query;
 using Cite.WebTools.CurrentPrincipal;
-using Terra.Gateway.App.Common.Auth;
 using Terra.Gateway.App.Service.AAI;
 
 namespace Terra.Gateway.App.Authorization
@@ -51,79 +49,6 @@ namespace Terra.Gateway.App.Authorization
 		public ISet<String> PermissionsOfContextRoles(IEnumerable<String> roles)
 		{
 			return this._permissionPolicyService.PermissionsOfContext(roles);
-		}
-
-
-		public async Task<HashSet<String>> ContextRolesForCollectionOfUser(String subjectId, Guid collectionId)
-		{
-			Dictionary<Guid, HashSet<String>> rolesByCollection= await this.ContextRolesForCollectionOfUser(subjectId, [collectionId]);
-			if(rolesByCollection == null || !rolesByCollection.ContainsKey(collectionId)) return Enumerable.Empty<String>().ToHashSet();
-			return rolesByCollection[collectionId];
-		}
-
-
-		public async Task<Dictionary<Guid, HashSet<String>>> ContextRolesForCollectionOfUser(String subjectId, IEnumerable<Guid> collectionIds)
-		{
-			if (collectionIds == null || !collectionIds.Any()) return new Dictionary<Guid, HashSet<string>>();
-
-			HashSet<Guid> collectionIdsMap = collectionIds.ToHashSet();
-
-			List<ContextGrant> grants = await this._aaiService.LookupUserContextGrants(subjectId);
-			Dictionary<Guid, HashSet<String>> rolesByCollection = grants
-				.Where(x => x.TargetType == ContextGrant.TargetKind.Collection && collectionIdsMap.Contains(x.TargetId))
-				.ToDictionaryOfList(x => x.TargetId)
-				.ToDictionary(x => x.Key, x => x.Value.Select(y => y.Role).ToHashSet());
-			return rolesByCollection;
-		}
-
-		public async Task<HashSet<String>> ContextRolesForCollectionOfUserGroup(String groupId, Guid collectionId)
-		{
-			Dictionary<Guid, HashSet<String>> rolesByCollection = await this.ContextRolesForCollectionOfUserGroup(groupId, [collectionId]);
-			if (rolesByCollection == null || !rolesByCollection.ContainsKey(collectionId)) return Enumerable.Empty<String>().ToHashSet();
-			return rolesByCollection[collectionId];
-		}
-
-		public async Task<Dictionary<Guid, HashSet<String>>> ContextRolesForCollectionOfUserGroup(String groupId, IEnumerable<Guid> collectionIds)
-		{
-			if (collectionIds == null || !collectionIds.Any()) return new Dictionary<Guid, HashSet<string>>();
-
-			HashSet<Guid> collectionIdsMap = collectionIds.ToHashSet();
-
-			List<ContextGrant> grants = await this._aaiService.LookupUserGroupContextGrants(groupId);
-			Dictionary<Guid, HashSet<String>> rolesByCollection = grants
-				.Where(x => x.TargetType == ContextGrant.TargetKind.Collection && collectionIdsMap.Contains(x.TargetId))
-				.ToDictionaryOfList(x => x.TargetId)
-				.ToDictionary(x => x.Key, x => x.Value.Select(y => y.Role).ToHashSet());
-			return rolesByCollection;
-		}
-
-
-		public async Task<Dictionary<Guid, HashSet<String>>> EffectiveContextRolesForDatasetOfUser(String subjectId, IEnumerable<Guid> datasetIds)
-		{
-			if (datasetIds == null || !datasetIds.Any()) return new Dictionary<Guid, HashSet<string>>();
-
-			HashSet<Guid> datasetIdsMap = datasetIds.ToHashSet();
-			List<ContextGrant> grants = await this._aaiService.LookupUserContextGrants(subjectId);
-			Dictionary<Guid, HashSet<String>> rolesByDataset = grants
-				.Where(x => x.TargetType == ContextGrant.TargetKind.Dataset && datasetIdsMap.Contains(x.TargetId))
-				.ToDictionaryOfList(x => x.TargetId)
-				.ToDictionary(x => x.Key, x => x.Value.Select(y => y.Role).ToHashSet());
-
-			return rolesByDataset;
-		}
-
-		public async Task<Dictionary<Guid, HashSet<String>>> EffectiveContextRolesForDatasetOfUserGroup(String groupId, IEnumerable<Guid> datasetIds)
-		{
-			if (datasetIds == null || !datasetIds.Any()) return new Dictionary<Guid, HashSet<string>>();
-
-			HashSet<Guid> datasetIdsMap = datasetIds.ToHashSet();
-			List<ContextGrant> grants = await this._aaiService.LookupUserGroupContextGrants(groupId);
-			Dictionary<Guid, HashSet<String>> rolesByDataset = grants
-				.Where(x => x.TargetType == ContextGrant.TargetKind.Dataset && datasetIdsMap.Contains(x.TargetId))
-				.ToDictionaryOfList(x => x.TargetId)
-				.ToDictionary(x => x.Key, x => x.Value.Select(y => y.Role).ToHashSet());
-
-			return rolesByDataset;
 		}
 	}
 }
--- src/Terra.Gateway.App/Authorization/IAuthorizationContentResolver.cs
+++ src/Terra.Gateway.App/Authorization/IAuthorizationContentResolver.cs
@@ -9,19 +9,5 @@
 		Task<Boolean> HasPermission(params String[] permissions);
 
 		ISet<String> PermissionsOfContextRoles(IEnumerable<String> roles);
-
-
-		Task<HashSet<String>> ContextRolesForCollectionOfUser(String subjectId, Guid collectionId);
-
-
-		Task<Dictionary<Guid, HashSet<String>>> ContextRolesForCollectionOfUser(String subjectId, IEnumerable<Guid> collectionIds);
-
-		Task<HashSet<String>> ContextRolesForCollectionOfUserGroup(String groupId, Guid collectionId);
-
-		Task<Dictionary<Guid, HashSet<String>>> ContextRolesForCollectionOfUserGroup(String groupId, IEnumerable<Guid> collectionIds);
-
-		Task<Dictionary<Guid, HashSet<String>>> EffectiveContextRolesForDatasetOfUser(String subjectId, IEnumerable<Guid> datasetIds);
-
-		Task<Dictionary<Guid, HashSet<String>>> EffectiveContextRolesForDatasetOfUserGroup(String groupId, IEnumerable<Guid> datasetIds);
 	}
 }
